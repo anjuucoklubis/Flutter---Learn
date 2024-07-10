ae881a1de8c22c45b1e43d8af916c98231db69aa
Anju Lubis <78084093+anjuucoklubis@users.noreply.github.com>
2024-07-10 17:50:52 +0700

    02.-FlutterLearn-TextWidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,20 @@ void main() {
   runApp(new MyApp());
 }
 
+// Catatan 1 ====================
 // cara bikin class MyApp dengan shorcut "stl" lalu enter
 // Stl adalah Stateless
 // Widged ada 2 macam : Stateless widget dan Statefull widget
 // dibawah ini adalah class MyApp dengan turunan Stateless  widget
 //material app = sebuah widget yg berisi data2 yang diperlukan oleh aplikasi yang menggunakan material desain
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     //method build
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text("Aplikasi Hello Wolrd"),
-//         ),
-//         body: Text("Hello World"),
-//       ),
-//     );
-//   }
-// }
 
+// Catatan 2 ==================== LASTEST
+// Widget Container digunakan untuk membungkus widget, dimana nantinya dapat digunakan untuk membuat padding, margin dll
+// MaxLines = digunakan untuk memberi maximal line pada cointaner
+// Overflow = digunakan untuk mengatur bagaimana text ditampilkan  jika dia terpotong
+// softWrap = jika true diakan disesuaikan, kalau false dipotong
+// textAlign = untuk mengatur text apakah rata kiri atau rata kanan
+// Style = mengatur gaya dari text
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -36,7 +30,23 @@ class MyApp extends StatelessWidget {
           title: Text("Aplikasi Hello World"),
           backgroundColor: Color(0xFF42A5F5),
         ),
-        body: Center(child: Text("Hello World")),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 120,
+                child: Text(
+                  "Saya sedang melatih kemampuan flutter saya.",
+                  // maxLines: 2,
+                  // overflow: TextOverflow.clip,
+                  // softWrap: false,
+                  //textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ))),
       ),
     );
   }
